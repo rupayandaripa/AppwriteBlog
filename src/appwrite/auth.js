7d8ec0277d92cd66335f06900524ddb3d5c55bdb
iamrupayandaripa <rupayandaripa@gmail.com>
2024-10-06 12:35:28 +0530

    first commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,67 @@
+import conf from '../conf/conf.js'
+import {Client , Account , ID} from 'appwrite'
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+                  .setEndpoint(conf.appwriteUrl)
+                  .setProject(conf.appwriteProjectId)
+
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email , password , name}) {
+        
+        try {
+            const userAccount = await this.account.create(ID.unique() , email , password , name)
+            if(userAccount) {
+                console.log("Hello")
+                return this.login({email , password})
+            }
+            else {
+                return userAccount
+            }
+        }
+        catch(error) {
+            console.log(error)
+            throw error
+        }
+    }
+
+    async login({email , password}) {
+        try{
+            await this.account.createEmailPasswordSession(email,password)
+        }
+        catch(error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get()
+        }
+        catch(error) {
+            throw error
+        }
+
+        return null
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions()
+        }
+        catch(error) {
+            throw error
+        }
+    }
+}
+
+const authService = new AuthService()
+
+
+export default authService
---
+++ src/components/index.js
@@ -0,0 +1,34 @@
+import Header from "./header/header";
+import Footer from "./footer/footer";
+import Container from "./container/container.jsx";
+import Logo from "./logo";
+import LogoutBtn from "./header/LogoutBtn";
+import Button from "./Button";
+import Input from "./input.jsx"
+import RTE from './RTE.jsx'
+import Signup from "./SignUp.jsx";
+import Login from "./Login.jsx"
+import Post from './Post.jsx'
+import PostForm from './post-form/PostForm.jsx'
+import PostCard from './PostCard.jsx'
+import Protected from './AuthLayout.jsx'
+import Select from './Select.jsx'
+
+
+export {
+    Header,
+    Footer,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    Signup,
+    Login,
+    Post,
+    PostForm,
+    PostCard,
+    Protected,
+    RTE,
+    Select,
+    Container
+}
